47d364da110c6c8aa7ce61424a97c5b47bc2bc9c
Eve <e_sotnik@bk.ru>
2021-03-03 12:15:57 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TimeAngle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите точное время.");
+            // время точное, т.е. минутная стрелка всегда на 12
+            var s = Console.ReadLine();
+            int H = int.Parse(s);
+            int oneHangle = 360 / 12;
+            int res = Math.Abs((H / 6 * 12 - H) * oneHangle);
+            Console.WriteLine(res);
+        }
+    }
+}
